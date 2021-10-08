6518f2d0cbff066f9d2978c2cd4edc7b43e5e603
rnssol <mustafaawwal@rnssol.com>
2021-10-08 12:43:48 +0500

    * Fix: removed unused files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Button } from "../@next/components/atoms";
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Button>Something</Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
